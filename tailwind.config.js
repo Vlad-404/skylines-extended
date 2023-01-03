c211c09711d7458d99bee9c3139ad3698c2188b2
Vlad-404 <vmijat21@gmail.com>
2023-01-03 17:19:52 +0100

    Add html,css files and Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    '*.html'
+  ],
+  content: [],
+  theme: {
+    extend: {
+      appearance: ['hover', 'focus']
+    },
+  },
+  plugins: [],
+}
